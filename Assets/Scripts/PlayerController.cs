f9530ecfc7319a1c02ac5582ce24063da069cb97
Lucrezia Tasso <tassolucrezia@gmail.com>
2026-07-26 20:53:00 +0200

    animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
+    private Animator animator;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -17,11 +18,13 @@ public class PlayerController : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
+        animator.SetFloat("Speed", movement.magnitude);
 
         if (movement != Vector3.zero)
         {
             transform.rotation = Quaternion.LookRotation(movement);
-            transform.Translate(Vector3.forward * speed * Time.deltaTime); 
+    
         }
+         transform.Translate(movement * speed * Time.deltaTime, Space.World); 
     }
 }
