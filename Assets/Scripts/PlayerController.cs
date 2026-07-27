42474c3f008603d0d7893706cc6ddbec8eb87af1
Lucrezia Tasso <tassolucrezia@gmail.com>
2026-07-27 10:48:23 +0200

    run

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float runSpeed = 10f;
     private Animator animator;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -14,17 +15,25 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+     float horizontalInput = Input.GetAxis("Horizontal");
+     float verticalInput = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
-        animator.SetFloat("Speed", movement.magnitude);
+     Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
 
-        if (movement != Vector3.zero)
-        {
-            transform.rotation = Quaternion.LookRotation(movement);
-    
-        }
-         transform.Translate(movement * speed * Time.deltaTime, Space.World); 
+     bool isMoving = horizontalInput != 0 || verticalInput != 0;
+     bool isRunning = (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) && isMoving;
+
+     animator.SetFloat("Speed", movement.magnitude);
+     animator.SetBool("isWalking", isMoving);
+     animator.SetBool("isRunning", isRunning);
+
+    if (movement != Vector3.zero)
+    {
+     transform.rotation = Quaternion.LookRotation(movement);
+    }
+
+    float currentSpeed = isRunning ? runSpeed : speed;
+
+    transform.Translate(movement * currentSpeed * Time.deltaTime, Space.World);
     }
 }
