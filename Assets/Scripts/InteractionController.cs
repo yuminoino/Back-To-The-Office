0117ff0d0ec00e07a7da93b30888c2f9f0cb3638
Lucrezia Tasso <tassolucrezia@gmail.com>
2026-07-28 23:15:33 +0200

    ui

--- Assets/Scripts/InteractionController.cs
+++ Assets/Scripts/InteractionController.cs
@@ -1,31 +1,26 @@
 using UnityEngine;
-
 public class InteractionController : MonoBehaviour
 {
     public Camera PlayerCamera;
     public float InteractionDistance = 2f;
+    public LayerMask InteractionLayers;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+     if (Input.GetKeyDown(KeyCode.E))
+    {
+        Ray ray = new Ray(PlayerCamera.transform.position, PlayerCamera.transform.forward);
+
+        Debug.DrawRay(ray.origin, ray.direction * 10f, Color.red, 2f);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, 10f))
         {
-            Ray ray = new Ray(PlayerCamera.transform.position, PlayerCamera.transform.forward);
-
-            Debug.DrawRay(ray.origin, ray.direction * InteractionDistance, Color.red, 2f);
-
-            if (Physics.Raycast(ray, out RaycastHit hit, InteractionDistance))
-            {
-                Debug.Log("Colpito: " + hit.collider.name);
-
-                if (hit.collider.CompareTag("Inspectable"))
-                {
-                    Debug.Log("Interazione!");
-                }
-            }
-            else
-            {
-                Debug.Log("Nessun oggetto colpito");
-            }
+            Debug.Log("Colpito: " + hit.collider.name);
         }
+        else
+        {
+            Debug.Log("Non ho colpito nulla");
+        }
+    }
     }
 }
