d1b8769efaad586fd3f0a1e594505b7c5217fd2f
Lucrezia Tasso <tassolucrezia@gmail.com>
2026-08-02 19:17:25 +0200

    added

--- Assets/Scripts/InteractionZone.cs
+++ Assets/Scripts/InteractionZone.cs
@@ -3,11 +3,24 @@ using UnityEngine;
 public class InteractionZone : MonoBehaviour
 {
     public GameObject InteractionText;
+    public GameObject DialogueObject;
+
+    private bool playerInside;
+
+    void Update()
+    {
+        if (playerInside && Input.GetKeyDown(KeyCode.E))
+        {
+            InteractionText.SetActive(false);
+            DialogueObject.SetActive(true);
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            playerInside = true;
             InteractionText.SetActive(true);
         }
     }
@@ -16,7 +29,10 @@ public class InteractionZone : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+            playerInside = false;
+
             InteractionText.SetActive(false);
+            DialogueObject.SetActive(false);
         }
     }
 }
