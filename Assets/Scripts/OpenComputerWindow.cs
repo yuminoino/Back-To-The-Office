7b845b3359081d0d0511606d8ce60b5843321d1d
Lucrezia Tasso <tassolucrezia@gmail.com>
2026-08-09 21:30:20 +0200

    open

---
+++ Assets/Scripts/OpenComputerWindow.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class OpenComputerWindow : MonoBehaviour
+{
+    public GameObject ComputerWindow;
+
+    public void Open()
+    {
+        ComputerWindow.SetActive(true);
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
