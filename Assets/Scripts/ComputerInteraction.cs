4ee0d485c3c45b8e6c05cf955e7717d8f941d398
Lucrezia Tasso <tassolucrezia@gmail.com>
2026-08-09 19:28:05 +0200

    computer

--- Assets/Scripts/ComputerInteraction.cs
+++ Assets/Scripts/ComputerInteraction.cs
@@ -5,23 +5,102 @@ public class ComputerInteraction : MonoBehaviour
     public GameObject ComputerUI;
     public Transform CameraPoint;
     public Camera PlayerCamera;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public PlayerController PlayerController;
+    public CameraController CameraController;
+
+    public RectTransform MonitorArea;
+
+    private bool playerInside;
+    private bool computerOpen;
+
+    private Vector3 originalCameraPosition;
+    private Quaternion originalCameraRotation;
+
     void Update()
     {
-        
+        if (playerInside && !computerOpen && Input.GetKeyDown(KeyCode.E))
+        {
+            OpenComputer();
+        }
+
+        if (computerOpen)
+        {
+            LimitCursor();
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                CloseComputer();
+            }
+        }
     }
-    public void Interact()
+
+    private void OnTriggerEnter(Collider other)
     {
+        if (other.CompareTag("Player"))
+        {
+            playerInside = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInside = false;
+        }
+    }
+
+    private void OpenComputer()
+    {
+        originalCameraPosition = PlayerCamera.transform.position;
+        originalCameraRotation = PlayerCamera.transform.rotation;
+
         PlayerCamera.transform.position = CameraPoint.position;
         PlayerCamera.transform.rotation = CameraPoint.rotation;
 
         ComputerUI.SetActive(true);
+
+        PlayerController.enabled = false;
+        CameraController.enabled = false;
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        computerOpen = true;
+    }
+
+    private void CloseComputer()
+    {
+        ComputerUI.SetActive(false);
+
+        PlayerCamera.transform.position = originalCameraPosition;
+        PlayerCamera.transform.rotation = originalCameraRotation;
+
+        PlayerController.enabled = true;
+        CameraController.enabled = true;
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        computerOpen = false;
+    }
+
+    private void LimitCursor()
+    {
+        Vector3[] corners = new Vector3[4];
+        MonitorArea.GetWorldCorners(corners);
+
+        Vector2 min = RectTransformUtility.WorldToScreenPoint(null, corners[0]);
+        Vector2 max = RectTransformUtility.WorldToScreenPoint(null, corners[2]);
+
+        Vector3 mousePosition = Input.mousePosition;
+
+        mousePosition.x = Mathf.Clamp(mousePosition.x, min.x, max.x);
+        mousePosition.y = Mathf.Clamp(mousePosition.y, min.y, max.y);
+
+        Cursor.lockState = CursorLockMode.None;
+
+        UnityEngine.InputSystem.Mouse.current.WarpCursorPosition(mousePosition);
     }
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
