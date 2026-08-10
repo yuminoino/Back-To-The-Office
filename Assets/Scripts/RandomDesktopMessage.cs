a3c8432cd957a37233304629c859a77220ee4055
Lucrezia Tasso <tassolucrezia@gmail.com>
2026-08-10 18:14:32 +0200

    Update RandomDesktopMessage.cs

--- Assets/Scripts/RandomDesktopMessage.cs
+++ Assets/Scripts/RandomDesktopMessage.cs
@@ -1,7 +1,30 @@
 using UnityEngine;
+using TMPro;
 
 public class RandomDesktopMessage : MonoBehaviour
 {
+    public TextMeshproUI RandomText;
+    public string[] Messages =
+    {
+        "DON'T bE PESSIMISTIC!",
+        "YOU HAVE A FrIEND HERE",
+        "YOU SEE EVERYTHING IN GRaY",
+        "vOID",
+        "YOU CAN'T eXIT",
+        "HELLO?",
+        "CAN YOU HEAR ME?",
+        "I MISS WHO I USED TO BE",
+        "THIS PLACE IS STILL HERE. THEY AREN'T",
+        "DESPITE EVERYTHING, IT'S STILL YOU",
+    };
+
+    public void ShowRandomMessage()
+    {
+        int randomIndex = Random.Range(0, Messages.Length);
+
+        RandomText.text = Messages[randomIndex];
+        RandomText.gameObject.SetActive(true);
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
