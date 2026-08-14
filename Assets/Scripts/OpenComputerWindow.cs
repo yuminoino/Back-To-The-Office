23381be28081fdbb3b2df7601efef42a3bd52cad
Lucrezia Tasso <tassolucrezia@gmail.com>
2026-08-14 17:05:07 +0200

    password

--- Assets/Scripts/OpenComputerWindow.cs
+++ Assets/Scripts/OpenComputerWindow.cs
@@ -1,22 +1,25 @@
 using UnityEngine;
+using TMPro;
 
 public class OpenComputerWindow : MonoBehaviour
 {
     public GameObject ComputerWindow;
+    public GameObject PasswordWindow;
+    public TMP_InputField PasswordInput;
+
+    private string Password = "brave";
 
     public void Open()
     {
-        ComputerWindow.SetActive(true);
-    }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+        PasswordWindow.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CheckPassword()
     {
-        
+        if (PasswordInput.text == Password)
+        {
+            PasswordWindow.SetActive(false);
+            ComputerWindow.SetActive(true);
+        }
     }
 }
